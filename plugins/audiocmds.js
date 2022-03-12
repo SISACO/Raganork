e4deb5b6faabe7b26c778e85d18a3b3490c9713e
SISACO <54985885+SISACO@users.noreply.github.com>
2022-03-12 13:25:10 +0530

    Update audiocmds.js

--- plugins/audiocmds.js
+++ plugins/audiocmds.js
@@ -6,7 +6,7 @@ const Config = require('../config');
     skl.addCommand({pattern: 'audio', fromMe: true, desc: 'Gives audio commands' , dontAddCommandList: true }, async (message, match) => {
 
         
-        const cmd = "```Ayin```\n```Ayinu```\n```Bgm```\n```Bot```\n```Bye```\n```Good night```\n```Hello```\n```Hi```\n```Neymar```\n```Pm```\n```Sed```\n```alive```\n```assist```\n```ban```\n```bgm```\n```bot```\n```converting```\n```fake```\n```fork```\n```fuck```\n```music```\n```myre```\n```njan```\n```number```\n```oombi```\n```poda```\n```raganork```\n```remove```\n```reply```\n```sed```\n```subscribe```\n```xxxtentation```";
+        const cmd = "```Juan```\n```Ayinu```\n```Bgm```\n```Bot```\n```Bye```\n```Good night```\n```Hello```\n```Hi```\n```Neymar```\n```Pm```\n```Sed```\n```alive```\n```assist```\n```ban```\n```bgm```\n```bot```\n```converting```\n```fake```\n```fork```\n```fuck```\n```music```\n```myre```\n```njan```\n```number```\n```oombi```\n```poda```\n```raganork```\n```remove```\n```reply```\n```sed```\n```subscribe```\n```xxxtentation```";
         const {data} = await axios(`https://gist.githubusercontent.com/SISACO/e29413927f93b5e8e9a1fb9c4469f487'`)
         const { souravkl11 } = data
         await message.client.sendMessage(message.jid, cmd, MessageType.text, { quoted: message.data });
@@ -16,7 +16,7 @@ const Config = require('../config');
 if (Config.WORKTYPE == 'public') {
     skl.addCommand({pattern: 'audio', fromMe: false, desc: 'Gives audio commands' , dontAddCommandList: true }, async (message, match) => {
 
-        const cmd = "```Ayin```\n```Ayinu```\n```Bgm```\n```Bot```\n```Bye```\n```Good night```\n```Hello```\n```Hi```\n```Neymar```\n```Pm```\n```Sed```\n```alive```\n```assist```\n```ban```\n```bgm```\n```bot```\n```converting```\n```fake```\n```fork```\n```fuck```\n```music```\n```myre```\n```njan```\n```number```\n```oombi```\n```poda```\n```raganork```\n```remove```\n```reply```\n```sed```\n```subscribe```\n```xxxtentation```";
+        const cmd = "```Juan```\n```Ayinu```\n```Bgm```\n```Bot```\n```Bye```\n```Good night```\n```Hello```\n```Hi```\n```Neymar```\n```Pm```\n```Sed```\n```alive```\n```assist```\n```ban```\n```bgm```\n```bot```\n```converting```\n```fake```\n```fork```\n```fuck```\n```music```\n```myre```\n```njan```\n```number```\n```oombi```\n```poda```\n```raganork```\n```remove```\n```reply```\n```sed```\n```subscribe```\n```xxxtentation```";
         const {data} = await axios('https://gist.githubusercontent.com/SISACO/e29413927f93b5e8e9a1fb9c4469f487')
         const { souravkl11 } = data
         await message.client.sendMessage(message.jid, cmd, MessageType.text, { quoted: message.data });
